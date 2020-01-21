12bcce29ba7c7b4e4af80ca91c0becad4973b5b3
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-01-21 07:51:51 +0200

    ADD -> tester script

---
+++ autotrader/test.py
@@ -0,0 +1,38 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import time
+import re
+import sys
+
+sys.path.insert(1, "E:\\projects\\python\\data_science\\autotrader")
+saveFile = "E:\\projects\\python\\data_science\\autotrader\\saveFile.txt"
+
+def pageDownload(URL):
+    return urlopen(URL).read().decode("utf-8")
+
+def savePage(URL, file):
+    page = urlopen(URL).read().decode("utf-8")
+    proc = open(file, "w")
+    proc.write(page)
+    proc.close()
+    return page
+
+def extractHREF():
+    with open(saveFile) as file:
+        text = file.read()
+        soup = BeautifulSoup(text, "html.parser")
+        proc = open("URLs.txt" , "a")
+        for tag in soup.select("div.e-available>a", href=True):
+            hrefTag = tag["href"]
+            proc.write("https://www.autotrader.co.za{}\n".format(hrefTag))
+        proc.close()
+
+if __name__ == "__main__":
+    for i in range(155,165):
+        target = "https://www.autotrader.co.za/cars-for-sale/western-cape/p-9?pagenumber={}&price=50001-to-200000".format(i)
+        savePage(target, saveFile)
+        extractHREF()
+        print("No: {} => Complete".format(i))
+        time.sleep(10)
+
+#for i in range(len(pagArr))
