14e486bccbccb822921635ed1b307c510f97eb98
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2020-01-24 07:13:28 +0200

    UPDATE -> data encoding

--- autotrader/getURL.py
+++ autotrader/getURL.py
@@ -11,6 +11,8 @@ saveFile = "FILE.txt"
 userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
 headers = {"User-Agent": userAgent}
 values = {"name": "Chrome"}
+data = urllib.parse.urlencode(values)
+data = data.encode("ascii")
 
 def pageDownload(URL):
     return urlopen(URL).read().decode("utf-8")
