bf06fc1f8bbeb328a4f00e94d825fc46fea4b16a
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2020-01-26 23:29:49 +0200

    ADD -> dummyValues

--- autotrader/mongoUpstream.py
+++ autotrader/mongoUpstream.py
@@ -4,3 +4,33 @@ import uuid
 client = pymongo.MongoClient("localhost", 27017)
 db = client["TheHoarder"]
 
+dummyCars = [
+    {
+        "autotrader_id": "autotrader-{}".format(uuid.uuid4()),
+        "site": "autotrader.co.za",
+        "brand": "Audi",
+        "title": "2010 Audi A3 3-Door 1.8T Ambition",
+        "area": "Montague Gardens",
+        "price": "R 78 000",
+        "range": "R50k to R100k"
+    },
+    {
+        "autotrader_id": "autotrader-{}".format(uuid.uuid4()),
+        "site": "autotrader.co.za",
+        "brand": "Audi",
+        "title": "2008 Audi A4 1.8T Ambition",
+        "area": "Montague Gardens",
+        "price": "R 60 000",
+        "range": "R50k to R100k"
+    },
+    {
+        "autotrader_id": "autotrader-{}".format(uuid.uuid4()),
+        "site": "autotrader.co.za",
+        "brand": "BMW",
+        "title": "2010 BMW 1 Series 120i Convertible Auto",
+        "area": "Montague Gardens",
+        "price": "R 169 900",
+        "range": "R100k to R200k"
+    }
+]
+
